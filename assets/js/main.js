06055a07744fcc23e76dc8dc5d47384d74276359
rodolforecordon <rodolfo@perithus.tech>
2021-09-12 00:45:38 -0300

    making adjustments to make the code more mordern following tips from especialist

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,66 @@
-// fórmula: IMC = peso / (altura * altura)
+const form = document.querySelector('#myForm');
 
-// document.querySelector('#formulario').
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-function calcularIMC(event) {
-  event.preventDefault();
-  
-  let peso = document.querySelector('#peso').value;
-  let altura = document.querySelector('#altura').value / 100;
-  let imc = (peso / (altura * altura)).toFixed(2);
+  const inputWeight = e.target.querySelector('#weight')
+  const inputHeight = e.target.querySelector('#height')
 
-  document.querySelector('#resultado').innerHTML = ''
+  const weight = Number(inputWeight.value);
+  const height = Number(inputHeight.value) / 100;
 
-  if (imc < 18.5) {
-    document.querySelector('#resultado').style.display = 'block';
-    document.querySelector('#resultado').innerHTML = `Seu IMC é ${imc} (Abaixo do peso)`;
-  } else if (imc >= 18.5 && imc < 25) {
-    document.querySelector('#resultado').style.display = 'block';
-    document.querySelector('#resultado').innerHTML = `Seu IMC é ${imc} (Peso normal)`;
-  } else if (imc >= 25 && imc < 30) {
-    document.querySelector('#resultado').style.display = 'block';
-    document.querySelector('#resultado').innerHTML = `Seu IMC é ${imc} (Sobrepeso)`;
-  } else if (imc >= 30 && imc < 35) {
-    document.querySelector('#resultado').style.display = 'block';
-    document.querySelector('#resultado').innerHTML = `Seu IMC é ${imc} (Obesidade Grau 1)`;
-  } else if (imc >= 35 && imc < 40) {
-    document.querySelector('#resultado').style.display = 'block';
-    document.querySelector('#resultado').innerHTML = `Seu IMC é ${imc} (Obesidade Grau 2)`;
-  } else if (imc >= 40) {
-    document.querySelector('#resultado').style.display = 'block';
-    document.querySelector('#resultado').innerHTML = `Seu IMC é ${imc} (Obesidade Grau 3)`;
+  if (!weight) {
+    setResult('Invalid weight.', false);
+    return;
   }
+
+  if (!height) {
+    setResult('Invalid height.', false);
+    return;
+  }
+
+  const bmi = getBmi(weight, height);
+  const bmiClass = getBmiClass(bmi)
+
+  const msg = `Your BMI is ${bmi} (${bmiClass}).`;
+
+  setResult(msg, true);
+})
+
+function getBmiClass(bmi) {
+  const bmiClass = ['Underweight', 'Healthy', 'Overweight',
+    'Obese (Class 1)', 'Obese - Class 2', 'Obese - Class 3'];
+
+  if (bmi >= 40) return bmiClass[5];
+  if (bmi >= 35) return bmiClass[4];
+  if (bmi >= 30) return bmiClass[3];
+  if (bmi >= 25) return bmiClass[2];
+  if (bmi >= 18.5) return bmiClass[1];
+  if (bmi < 18.5) return bmiClass[0];
+}
+
+function getBmi(weight, height) {
+  const bmi = weight / height ** 2;
+  return bmi.toFixed(2);
+}
+
+function createParagraph() {
+  const p = document.createElement('p');
+  return p;
+}
+
+function setResult(message, isValid) {
+  const result = document.querySelector('#result');
+  result.innerHTML = '';
+
+  const p = createParagraph();
+
+  if (isValid) {
+    p.classList.add('result-paragraph');
+  } else {
+    p.classList.add('bad');
+  }
+
+  p.innerHTML = message;
+  result.appendChild(p);
 }
