fafd4aab702bf49d86a2c3e63d44f1616cba4bd7
rodolforecordon <rodolfo@perithus.tech>
2021-09-15 01:25:44 -0300

    simplifying functions

--- assets/js/dateTime.js
+++ assets/js/dateTime.js
@@ -22,82 +22,109 @@ function finalDate(date) {
 
 function getMonthName(monthNum) {
   const monthNames = ['January','February','March','April','May','June','July','August','September','October','November','December']
-  switch (monthNum) {
-    case 0:
-      return monthNames[0];
-    case 1:
-      return monthNames[1];
-    case 2:
-      return monthNames[2];
-    case 3:
-      return monthNames[3];
-    case 4:
-      return monthNames[4];
-    case 5:
-      return monthNames[5];
-    case 6:
-      return monthNames[6];
-    case 7:
-      return monthNames[7];
-    case 8:
-      return monthNames[8];
-    case 9:
-      return monthNames[9];
-    case 10:
-      return monthNames[10];
-    case 11:
-      return monthNames[11];
-  }
+  return monthNames[monthNum]
 }
 
 function getWeekDay(weekDayNum) {
   const weekDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-  switch (weekDayNum) {
-    case 0:
-      return weekDays[0];
-    case 1:
-      return weekDays[1];
-    case 2:
-      return weekDays[2];
-    case 3:
-      return weekDays[3];
-    case 4:
-      return weekDays[4];
-    case 5:
-      return weekDays[5];
-    case 6:
-      return weekDays[6];
-  }
+  return weekDays[weekDayNum]
 }
 
 function getdaySuffix(dayNum) {
-  const sufixes = ['st','nd','rd','th']
-  switch (dayNum) {
+  const suffixes = ['st','nd','rd','th']
+  const dayLastDigit = toString(dayNum).slice(toString(dayNum).length - 1)
+  
+  switch (dayLastDigit) {
     case 1:
-      return sufixes[0];
+      return suffixes[0];
     case 2:
-      return sufixes[1];
+      return suffixes[1];
     case 3:
-      return sufixes[2];
-    case 11:
-      return sufixes[0];
-    case 12:
-      return sufixes[1];
-    case 13:
-      return sufixes[2];
-    case 21:
-      return sufixes[0];
-    case 22:
-      return sufixes[1];
-    case 23:
-      return sufixes[2];
-    case 31:
-      return sufixes[0];
+      return suffixes[2];
     default:
-      return sufixes[3];
+      return suffixes[3]
   }
 }
 
 function leftZero(number) {
   return number >=10 ? number : `0${number}`
 }
+
+
+// function getMonthName(monthNum) {
+//   const monthNames = ['January','February','March','April','May','June','July','August','September','October','November','December']
+//   switch (monthNum) {
+//     case 0:
+//       return monthNames[0];
+//     case 1:
+//       return monthNames[1];
+//     case 2:
+//       return monthNames[2];
+//     case 3:
+//       return monthNames[3];
+//     case 4:
+//       return monthNames[4];
+//     case 5:
+//       return monthNames[5];
+//     case 6:
+//       return monthNames[6];
+//     case 7:
+//       return monthNames[7];
+//     case 8:
+//       return monthNames[8];
+//     case 9:
+//       return monthNames[9];
+//     case 10:
+//       return monthNames[10];
+//     case 11:
+//       return monthNames[11];
+//   }
+// }
+
+// function getWeekDay(weekDayNum) {
+//   const weekDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+//   switch (weekDayNum) {
+//     case 0:
+//       return weekDays[0];
+//     case 1:
+//       return weekDays[1];
+//     case 2:
+//       return weekDays[2];
+//     case 3:
+//       return weekDays[3];
+//     case 4:
+//       return weekDays[4];
+//     case 5:
+//       return weekDays[5];
+//     case 6:
+//       return weekDays[6];
+//   }
+// }
+
+// function getdaySuffix(dayNum) {
+//   const sufixes = ['st','nd','rd','th']
+//   switch (dayNum) {
+//     case 1:
+//       return sufixes[0];
+//     case 2:
+//       return sufixes[1];
+//     case 3:
+//       return sufixes[2];
+//     case 11:
+//       return sufixes[0];
+//     case 12:
+//       return sufixes[1];
+//     case 13:
+//       return sufixes[2];
+//     case 21:
+//       return sufixes[0];
+//     case 22:
+//       return sufixes[1];
+//     case 23:
+//       return sufixes[2];
+//     case 31:
+//       return sufixes[0];
+//     default:
+//       return sufixes[3];
+//   }
+// }
